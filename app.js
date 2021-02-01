97515f1edb7c035d2d03c0d9c1eaacfab5da93c6
Marcelo <marcelosen4@gmail.com>
2021-02-01 20:11:33 -0300

    se agrega settings y router

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express();
 
 // settings
 app.set('port', 3000);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // middlewares
 
+// router
+const indexRouter = require('./routes/index.router');
+
 // routes
+app.use('/', indexRouter);
+
+// static files
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => console.log('Server on port', app.get('port')));
