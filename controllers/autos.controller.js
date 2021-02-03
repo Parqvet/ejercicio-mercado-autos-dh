9c7f5578fe20f3ee0096aa7d8c6942ef213870af
Marcelo <marcelosen4@gmail.com>
2021-02-03 08:53:29 -0300

    agregado funcion desde ejs en view detalle de autos para mostrar el detalle de cada una

--- controllers/autos.controller.js
+++ controllers/autos.controller.js
@@ -6,5 +6,14 @@ module.exports = {
             title: 'Nuestros Autos',
             autosDB
         });
+    },
+    show: (req, res) => {
+        const id = req.params.id;
+        const auto = autosDB.find(auto => auto.id == id);
+
+        res.render('autos-show', {
+            title: 'Vista de Detalle',
+            auto
+        })
     }
 }
--- routes/autos.router.js
+++ routes/autos.router.js
@@ -5,4 +5,7 @@ const autosController = require('../controllers/autos.controller');
 
 router.get('/', autosController.renderIndex);
 
+// llegar a esta ruta con un parametro
+router.get('/show/:id', autosController.show);
+
 module.exports = router;
