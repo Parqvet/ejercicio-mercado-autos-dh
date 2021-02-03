6478cf5fd436013cc6294a8f5be167165d0d8397
Marcelo <marcelosen4@gmail.com>
2021-02-03 08:23:08 -0300

    agregado view autos para e imagenes de las mismas

--- controllers/autos.controller.js
+++ controllers/autos.controller.js
@@ -1,7 +1,10 @@
+const autosDB = require('../data/autos');
+
 module.exports = {
     renderIndex: (req, res) => {
         res.render('autos', {
-            title: 'Nuestros Autos'
+            title: 'Nuestros Autos',
+            autosDB
         });
     }
 }
