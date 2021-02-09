8be606a2dff30c6d09dd3e63bc4bb32fb5151f90
Marcelo <marcelosen4@gmail.com>
2021-02-09 16:25:51 -0300

    se agregan enrutadores y controladores del panel de admin

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,7 +1,27 @@
-const adminDB = require('../data/autos');
+const autos = require('../data/autos');
 
 module.exports = {
     renderAdmin: (req, res) => {
         res.render('admin/index');
+    },
+    renderCarsList: (req, res) => {
+        res.render('admin/carsList', {
+            autos
+        });
+    },
+    carsCreate: (req, res) => {
+
+    },
+    carsStore: (req, res) => {
+        
+    },
+    carsEdit: (req, res) => {
+        
+    },
+    carsUpdate: (req, res) => {
+        
+    },
+    carsDelete: (req, res) => {
+        
     }
 }
--- routes/admin.router.js
+++ routes/admin.router.js
@@ -1,8 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 
-const { renderAdmin } = require('../controllers/admin.controller');
+const { renderAdmin, renderCarsList, carsCreate, carsStore, carsEdit, carsUpdate, carsDelete } = require('../controllers/admin.controller');
 
+// renderizas index del admin
 router.get('/', renderAdmin);
+
+// listar autos
+router.get('/autos/list', renderCarsList);
+
+// crear autos
+router.get('/autos/create', carsCreate);
+router.post('/autos/store', carsStore);
+
+// editar autos
+router.get('/autos/edit/:id', carsEdit);
+router.put('/autos/update/:id', carsUpdate);
+
+// eliminar autos
+router.delete('/autos/delete/:id', carsDelete);
 
 module.exports = router;
